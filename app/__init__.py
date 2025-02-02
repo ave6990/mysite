c7fc210ba038ea1c0a6ed07525cb649105fc415b
ave6990 <ave6990@ya.ru>
2025-02-02 10:04:01 +0500

    On branch dev  Changes to be committed: 	new file:   app/.forms.py.swp 	modified:   app/__init__.py 	new file:   app/forms.py 	modified:   app/routes.py 	modified:   app/templates/login.html 	modified:   app/templates/page.html 	new file:   config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,22 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import render_template 
+from flask import render_template, flash, redirect 
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
+@app.route('/index')
 def home():
-  return render_template('index.html', content='Hello from Flask!')
+  return render_template('index.html', title="Home", content='Hello from Flask!')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template('login.html')
+  form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for user {}, remember_me={}'.format(
+      form.username.data, form.remember_me.data))
+    return redirect('/index')
+  return render_template('login.html', title='Sign In', form=form)
 
 #if __name__ == "__main__":
 #  app.run(debug=True)
