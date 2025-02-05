48bb94c3e21a3bca5eadc5aac8aa132c7b19bbec
ave6990 <ave6990@ya.ru>
2025-02-05 18:53:17 +0500

    On branch dev  Changes to be committed: 	renamed:    app/.forms.py.swp -> app/.routes.py.swp 	modified:   app/routes.py 	new file:   app/templates/.page.html.swp 	modified:   app/templates/page.html

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import render_template, flash, redirect 
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-def home():
+def index():
   return render_template('index.html', title="Home", content='Hello from Flask!')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -15,7 +15,7 @@ def login():
   if form.validate_on_submit():
     flash('Login requested for user {}, remember_me={}'.format(
       form.username.data, form.remember_me.data))
-    return redirect('/index')
+    return redirect(url_for('index'))
   return render_template('login.html', title='Sign In', form=form)
 
 #if __name__ == "__main__":
