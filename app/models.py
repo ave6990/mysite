e8e550f1f2f8d2120d0d05ded79766dbc49bdf65
ave6990 <ave6990@ya.ru>
2025-05-06 15:16:08 +0500

    On branch dev  Changes to be committed: 	modified:   app/__init__.py 	modified:   app/models.py 	modified:   app/templates/page.html 	modified:   config.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
-class User(db.Model):
+class Condition(db.Model):
   id: so.Mapped[int] = so.mapped_column(primary_key=True)
-  username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-  email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
-  password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+  date: so.Mapped[datetime] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
+  temperature: so.Mapped[float] = so.mapped_column(sa.Float())
+  humidity: so.Mapped[float] = so.mapped_column(sa.Float())
+  pressure: so.Mapped[float] = so.mapped_column(sa.Float())
 
   def __repr__(self):
-    return '<User {}>'.format(self.username)
+    return f'<Condition {self.id}>'
--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY') or "unsecret-key"
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+  SECRET_KEY = os.environ.get('SECRET_KEY') or "enter"
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+  f'mariadb+mariadbconnector://ave:{SECRET_KEY}@192.168.0.118:3306/midb'
